683d579e03fba784a84ab78eb48e1318745abd86
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-07-09 21:22:04 -0500

    created view components and added soil data CHRON

--- server/server.js
+++ server/server.js
@@ -10,6 +10,7 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const chronRouter = require('./routes/chron.router')
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -24,6 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('api/chron', chronRouter);
 
 // Serve static files
 app.use(express.static('build'));
@@ -35,3 +37,4 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
 });
+
