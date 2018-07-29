64a665d1f7b069c51d232a68c435643582484779
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-07-28 23:47:12 -0500

    fixed sql file

--- database.sql
+++ database.sql
@@ -1,27 +1,26 @@
 CREATE TABLE person (
-    id SERIAL PRIMARY KEY,
+	id SERIAL,
     username VARCHAR (80) UNIQUE NOT NULL,
     password VARCHAR (1000) NOT NULL,
     auth_token VARCHAR (30),
     device_id VARCHAR (20),
-    zipcode INT REFERENCES zipcode(zipcode)
+    zipcode INT REFERENCES zipcode(zipcode),
+    PRIMARY KEY (username)
 );
 
 CREATE TABLE soil (
     event_ID SERIAL PRIMARY KEY,
     device_ID VARCHAR (80) UNIQUE NOT NULL,
     date VARCHAR (80) NOT NULL,
-    moisture INT NOT NULL
+    moisture INT
 );
 
 CREATE TABLE water (
-	event_ID SERIAL NOT NULL,
-	user_ID INT NOT NULL,
-	date VARCHAR (20) NOT NULL,
-	water_amount INT NOT NULL,
-	unit VARCHAR (5) NOT NULL,
-	PRIMARY KEY (event_ID),
-	FOREIGN KEY (user_ID) REFERENCES person(id)
+	event_ID SERIAL,
+	username VARCHAR (80) NOT NULL,
+	date VARCHAR (20),
+	water_amount INT,
+	PRIMARY KEY (event_ID)
 );
 
 CREATE TABLE zipcode (
@@ -29,15 +28,19 @@ CREATE TABLE zipcode (
 );
 
 CREATE TABLE device (
-	device_ID VARCHAR (20),
-	auth_token VARCHAR (30) REFERENCES person(auth_token),
-	user_ID INT REFERENCES person(id)
+	device_ID VARCHAR (40),
+	auth_token VARCHAR (30),
+	username VARCHAR (80),
+	notes VARCHAR (500),
+	id INT SERIAL,
+	PRIMARY KEY (device_ID)
 );
 
 CREATE TABLE rain (
 	event_ID SERIAL PRIMARY KEY,
 	zipcode INT REFERENCES zipcode(zipcode),
 	accumulation VARCHAR (20),
-	precip VARCHAR (20),
+	precip_type VARCHAR (20),
+	date VARCHAR (30),
 	icon VARCHAR (20)
 );
