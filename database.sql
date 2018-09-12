44a379113c1ef49ce5f95e3435a6c86bbe93b92a
jamesmeyers88 <jamesmeyers88@gmail.com>
2018-09-12 09:53:54 -0500

    fixed some db structure issues

--- database.sql
+++ database.sql
@@ -1,46 +1,42 @@
+
+
 CREATE TABLE person (
-	id SERIAL,
+    id SERIAL,
     username VARCHAR (80) UNIQUE NOT NULL,
     password VARCHAR (1000) NOT NULL,
     auth_token VARCHAR (30),
     device_id VARCHAR (20),
-    zipcode INT REFERENCES zipcode(zipcode),
     PRIMARY KEY (username)
 );
 
 CREATE TABLE soil (
     event_ID SERIAL PRIMARY KEY,
-    device_ID VARCHAR (80) UNIQUE NOT NULL,
+    device_ID VARCHAR (80) NOT NULL,
     date VARCHAR (80) NOT NULL,
     moisture INT
 );
 
 CREATE TABLE water (
-	event_ID SERIAL,
-	username VARCHAR (80) NOT NULL,
-	date VARCHAR (20),
-	water_amount INT,
-	PRIMARY KEY (event_ID)
-);
-
-CREATE TABLE zipcode (
-	zipcode INT NOT NULL	
+    event_ID SERIAL,
+    username VARCHAR (80) NOT NULL,
+    date VARCHAR (20),
+    water_amount INT,
+    PRIMARY KEY (event_ID)
 );
 
 CREATE TABLE device (
-	device_ID VARCHAR (40),
-	auth_token VARCHAR (30),
-	username VARCHAR (80),
-	notes VARCHAR (500),
-	id INT SERIAL,
-	PRIMARY KEY (device_ID)
+	id serial,
+    device_ID VARCHAR (40),
+    auth_token VARCHAR (30),
+    username VARCHAR (80),
+    notes VARCHAR (500),
+    PRIMARY KEY (device_ID)
 );
 
 CREATE TABLE rain (
-	event_ID SERIAL PRIMARY KEY,
-	zipcode INT REFERENCES zipcode(zipcode),
-	accumulation VARCHAR (20),
-	precip_type VARCHAR (20),
-	date VARCHAR (30),
-	icon VARCHAR (20)
+    event_ID SERIAL PRIMARY KEY,
+    accumulation VARCHAR (20),
+    precip_type VARCHAR (20),
+    date VARCHAR (30),
+    icon VARCHAR (20)
 );
